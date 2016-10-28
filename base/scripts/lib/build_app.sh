3e5a0b603ec07ff02cca8311593c74d94cacf2ec
Ori Ben Aharon <oriben2@gmail.com>
2016-10-28 12:34:57 +0300

    docker file

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,20 +8,22 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
-
 # npm i so all resources are available for the bundling
+echo "=> npm install (pre-build)"
 meteor npm install --unsafe-perm
-meteor build --directory $BUNDLE_DIR --server=http://localhost:3000 --unsafe-perm --server-only
 
+echo "=> meteor build"
+meteor build --directory $BUNDLE_DIR --server=http://localhost:3000 --unsafe-perm --server-only
 mv $BUNDLE_DIR/bundle /built_app
 
 # cleanup
+echo "=> cleanup"
 rm -rf $COPIED_APP_PATH
 rm -rf $BUNDLE_DIR
 rm -rf ~/.meteor
 rm /usr/local/bin/meteor
 
-
 # npm i on built app
-cd /built_app
+echo "=> npm install (post build)"
+cd /built_app/programs/server
 npm i --unsafe-perm
